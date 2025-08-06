69b72fce6bab0a4a17415ad9cfab5a83ce201188
tang <609712187@qq.com>
2025-08-06 13:58:56 +0930

    feat: building the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <>
+      <Button>
+        Click Me
+      </Button>
+    </>
+  )
+}
+
+export default App
